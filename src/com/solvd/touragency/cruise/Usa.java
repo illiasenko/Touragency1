48edc7782adb6ad0f5a329a7e0f7b34bb269aed8
Illia Senko <senko.illia14@gmail.com>
2020-12-14 22:10:06 +0200

    Touragency

---
+++ src/com/solvd/touragency/cruise/Usa.java
@@ -0,0 +1,23 @@
+package com.solvd.touragency.cruise;
+
+public class Usa extends America {
+
+    public Usa() {
+
+    }
+
+    public Usa(String place, int price, int time) {
+        super(place, price, time);
+    }
+
+    @Override
+    public void printInfo() {
+        System.out.println("CometoUsa");
+        System.out.println("place: " + getPlace());
+        System.out.println("price: " + getPrice());
+        System.out.println("time: " + getTime());
+        System.out.println(Yacht(true));
+    }
+
+}
+
